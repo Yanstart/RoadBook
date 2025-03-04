3ca6ba2dd6c2f6c79265c4543958ba6beb06e955
noel junior yando <yandofotsonoeljunior@gmail.com>
2025-03-04 00:02:12 +0000

    correction des bugs lié aux test avec jest dans github action

--- client/jest.config.mjs
+++ client/jest.config.mjs
@@ -1,8 +1,11 @@
 /* eslint-env node */
 export default {
-  preset: "jest-expo",
+  preset: 'jest-expo',
   transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)",
+    'node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
 };
