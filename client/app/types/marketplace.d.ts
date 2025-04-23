ff29c40556a70ed9815e952f1141ccc38a2b19c2
zawil-10 <wilfriedzanmo@gmail.com>
2025-04-23 22:21:54 +0200

    Marketplace ajoute et affichage des données

---
+++ client/app/types/marketplace.d.ts
@@ -0,0 +1,10 @@
+interface MarketplaceItem {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  sellerName: string;
+  sellerId: string;
+  imageUrl: string;
+  createdAt: Date;
+}
