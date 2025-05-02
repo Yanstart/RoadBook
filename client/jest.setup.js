6c8f7d9277a34698eb9e6457f5270f8003b1d6cb
ajkll <HE202326@students.ephec>
2025-05-02 20:57:06 +0200

    environment de test + premiers test + artefact de test covering dans le pipeline ci/cd bis 3

--- client/jest.setup.js
+++ client/jest.setup.js
@@ -1,4 +1,4 @@
-// jest.setup.js
+/* global jest, process */
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
