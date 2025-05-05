6c2563baaa3fb75d5a9913bdeaa34fc9fdd03a15
ajkll <HE202326@students.ephec>
2025-05-05 14:46:53 +0200

    chore(release): 2.1.0

--- client/app.config.js
+++ client/app.config.js
@@ -3,7 +3,7 @@ module.exports = {
   expo: {
     name: "Roadbook",
     slug: "Roadbook",
-    version: "2.0.0",
+    version: "2.1.0",
     orientation: "portrait",
     icon: "app/assets/images/icon.png",
     scheme: "myapp",
